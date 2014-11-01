bd9465a105d7a2186d1b6a476131dfc38673ac6b
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-01 11:36:02 -0600

    CFE_Controller Class Send Report Email Method
    The send report email method was now added as a protected method of the
    CFE Controller class. Now any method of any class that extends CFE
    Controller can make a call to send an email.
    
    Parameters:
    email,message.

--- application/core/CFE_Controller.php
+++ application/core/CFE_Controller.php
@@ -35,5 +35,43 @@ class CFE_Controller extends CI_Controller {
 		$this->load->library('session');
 		$this->session->sess_destroy();
 	}
+
+	protected function send_report_email($email,$comment)
+	{
+
+		$data['comment'] =  $comment;
+
+		$date = new DateTime();
+
+		$data['date'] = $date->format('Y-m-d'); 
+
+		$data['time'] = $date->format('H:i'); 
+
+		$this->load->library('email');
+				
+		$config = array(
+		    'protocol'  => 'smtp',
+		    'smtp_host' => 'email-smtp.us-west-2.amazonaws.com',
+		    'smtp_user' => 'AKIAJQEBYV6CQWDDLRMA',
+		    'smtp_pass' => 'AkcJTy/jsFMy0Uu6gC0VPRVAuTNl3CpWwv+Lx7qZ4KH1',
+		    'smtp_port' => 465,
+		    'newline'   => "\r\n",
+		    'mailtype' => 'html',
+		    'smtp_crypto' => 'ssl'
+		);
+		
+		$this->email->initialize($config);
+		
+	    $this->email->clear();
+	
+	    $this->email->to($email);
+	    $this->email->from('hola@retofutbol.com', 'CFE');
+	    $this->email->subject("Reporte de CFE");
+		
+		$msg = $this->load->view('estatus_reporte',$data,TRUE);
+		
+		$this->email->message($msg);
+		$this->email->send();
+	}
 	
 }
