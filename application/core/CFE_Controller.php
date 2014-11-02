866c868fb3d7473ea596856d46a58719aadcbc0e
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-02 14:25:46 -0600

    CFE_ObtenetEstados SOAP Connects Successfully
    The connection through the SOAP client is successful. However, the web
    service provided by CFE has an unsuccessful response:
    
    {"ObtenerEstadosResult":{"IdEstatusOperacion":"ValoresSeguridadIncorrect
    os","MensajeEstatus":"Los valores para revision de seguridad recibidos
    estan incorrectos o incompletos, favor de verificar cada valor enviado
    o notificarlo a un Administrador.","ListadoEstados":null}}

--- application/core/CFE_Controller.php
+++ application/core/CFE_Controller.php
@@ -73,5 +73,71 @@ class CFE_Controller extends CI_Controller {
 		$this->email->message($msg);
 		$this->email->send();
 	}
+
+	protected function CFE_ObtenerEstados()
+	{
+		$AccesoCliente = new stdClass();
+
+		$AccesoCliente->UsuarioMovil		=	"usrReto071";
+		$AccesoCliente->PasswordMovil	=	"@R3t0C3r053t3nt4yUn0@";
+		$AccesoCliente->SistemaOperativo	=	"Ubuntu";
+		$AccesoCliente->VersionSO		=	"14.04.1 LTS";
+		$AccesoCliente->TipoEquipo		=	"AWS EC2 Instance";
+		$AccesoCliente->ModeloEquipo		=	"t2 micro";
+		$AccesoCliente->ResolucionEquipo	=	"not available";
+		$AccesoCliente->IP				=	"54.69.204.188";
+		$AccesoCliente->Ubicacion		=	"not available";
+
+		$hashString = "usrReto071@R3t0C3r053t3nt4yUn0@Ubuntu14.04.1 LTSAWS EC2 Instancet2 micronot available54.69.204.188not availablevlw3xMqy9LNjQMs5rE4z";
+
+		$AccesoCliente->Hash				=	sha1($hashString);
+
+
+		$client = new SoapClient("http://aplicaciones.cfe.gob.mx/WebServices/CFEMovil/CFEMovil.svc?wsdl");
+
+		// $client->__setLocation('http://aplicaciones.cfe.gob.mx/WebServices/CFEMovil/CFEMovil.svc?wsdl/basic');
+
+		$response = $client->ObtenerEstados(array( "Acceso" => $AccesoCliente));
+
+		echo json_encode($response);
+
+		// echo($client);
+
+		// $CFE_ObtenerEstadosParams = array
+		// (
+		// 	"Acceso" => $AccesoCliente
+		// );
+
+		// return $this->send_http_request(CFE_URLS::SEL_ObtenerEstados,$CFE_ObtenerEstadosParams);
+
+	}
+
+
+	private function send_http_request($url , $data) 
+	{
+	    $request = json_encode($data);
+	 
+	    $ch = curl_init();
+
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_HEADER, true);
+	    curl_setopt($ch, CURLOPT_POST, true);
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+	 
+	    $response = curl_exec($ch);
+	    $info = curl_getinfo($ch);
+	    curl_close($ch);
+
+	    return $response;
+	}
+
+
 	
+}
+
+abstract class CFE_URLS
+{
+	const privateKey = "vlw3xMqy9LNjQMs5rE4z";
+    const SEL_ObtenerEstados = "http://aplicaciones.cfe.gob.mx/WebServices/CFEMovil/CFEMovil/ObtenerEstados";
 }
