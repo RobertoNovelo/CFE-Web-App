e0476c6fab12c8d63c796d2de6316b2ff881299c
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-03 18:07:59 -0600

    Set Worker Response JSON
    JSON is now returned from index method

--- application/controllers/set/worker.php
+++ application/controllers/set/worker.php
@@ -2,40 +2,40 @@
 
 class worker extends CFE_Controller {
 
-	/**
-	 * Get Worker controller.
-	 * 
-	 * This controller will process queries demanding worker data.
-	 *
-	 * Maps to the following URL:
-	 * 		/get/worker.php
-	 *
-	 */
-	
-	public function position()
-	{
-		$workerID	= $this->input->post('workerID');
-        $lat		= $this->input->post('lat');
-        $lng		= $this->input->post('lng');
+    /**
+     * Get Worker controller.
+     * 
+     * This controller will process queries demanding worker data.
+     *
+     * Maps to the following URL:
+     *      /get/worker.php
+     *
+     */
+    
+    public function position()
+    {
+        $workerID   = $this->input->post('workerID');
+        $lat        = $this->input->post('lat');
+        $lng        = $this->input->post('lng');
         
         if($workerID && $lat && $lng)
         {
-	        $this->load->model('set/set_worker');
-	        
-	        $this->set_worker->position($workerID,$lat,$lng);
-	        
-        	$response['requestStatus'] = 'OK';	
+            $this->load->model('set/set_worker');
+            
+            $this->set_worker->position($workerID,$lat,$lng);
+            
+            $response['requestStatus'] = 'OK';  
         }
         else
         {
-        	$response['requestStatus'] = 'NOK1';	
+            $response['requestStatus'] = 'NOK1';    
         }
         
         echo json_encode($response);
-	}
+    }
 
    
-	 public function assign()
+     public function assign()
     {
         $workerID       = $this->input->post('workerID');
         $reportTicket   = $this->input->post('reportTicket');
@@ -56,22 +56,22 @@ class worker extends CFE_Controller {
         
         echo json_encode($response);
     }
-	
-	public function index()
-	{
-		$userName	= $this->input->post('userName');
-        $password	= $this->input->post('password');
-        $name		= $this->input->post('name');
-        $fLastName	= $this->input->post('fLastName');
-        $sLastName	= $this->input->post('sLastName');
+    
+    public function index()
+    {
+        $userName   = $this->input->post('userName');
+        $password   = $this->input->post('password');
+        $name       = $this->input->post('name');
+        $fLastName  = $this->input->post('fLastName');
+        $sLastName  = $this->input->post('sLastName');
 
-        $key		= 'nkobvcevl7o87dfho9386qrho6b7eorato323kd9';
+        $key        = 'nkobvcevl7o87dfho9386qrho6b7eorato323kd9';
         
         
         //Validate inputs used when session==1;
         if ($userName && $password && $name && $fLastName && $sLastName)
         {
-			$this->load->library('session');
+            $this->load->library('session');
         
             $this->load->model('worker/worker_auth_model');
             
@@ -79,35 +79,37 @@ class worker extends CFE_Controller {
 
             if(!$user)
             {
-            	$encryptedPass = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $password, MCRYPT_MODE_CBC, md5(md5($key))));
-		
-            	$newWorker = array
-            	(
-            		'userName'	=> $userName,
-            		'password'	=> $encryptedPass,
-            		'name'		=> $name,
-            		'sessID'	=> uniqid(),
-            		'fLastName'	=> $fLastName,
-            		'sLastName'	=> $sLastName,
-            	);
-            	
-            	$this->worker_auth_model->create_worker($newWorker);
-            	
-            	echo 'OK';
-		
+                $encryptedPass = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $password, MCRYPT_MODE_CBC, md5(md5($key))));
+        
+                $newWorker = array
+                (
+                    'userName'  => $userName,
+                    'password'  => $encryptedPass,
+                    'name'      => $name,
+                    'sessID'    => uniqid(),
+                    'fLastName' => $fLastName,
+                    'sLastName' => $sLastName,
+                );
+                
+                $this->worker_auth_model->create_worker($newWorker);
+                
+                $response['requestStatus'] = 'OK';
+        
             }
             else
             {
-	            echo 'NOK2';
+                $response['requestStatus'] = 'NOK2';
             }
         }
         else
         {
-	        echo 'NOK1';
+            $response['requestStatus'] = 'NOK1';
         }
-	}
-	
-	
+
+        echo json_encode($response);
+    }
+    
+    
 }
 
 /* End of file auth.php */
